06f9e68ccfb5f0633a35086a8a7fc3a88dbe1f49
peiliuzhen <853415458@qq.com>
2017-06-19 14:36:26 +0800

    commit

---
+++ gitDemo/gitDemo/ViewController.m
@@ -0,0 +1,34 @@
+//
+//  ViewController.m
+//  gitDemo
+//
+//  Created by plz on 2017/6/19.
+//  Copyright © 2017年 com.bwsctv. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    self.view.backgroundColor=[UIColor yellowColor];
+    
+    self.title=@"测试";
+    
+    NSLog(@"github");
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
